637119dca37bcfe0773d178de3a8d3b80ab1b02f
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-05 15:42:37 +0300

    Ex_005(If_else)

---
+++ Example_005(If_else)/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя:");
+string user_name = Console.ReadLine();
+if (user_name.ToLower() == "маша"){
+    Console.WriteLine("Ура, это же МАША!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(user_name);
+}
