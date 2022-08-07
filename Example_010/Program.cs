01302a81dbeb453e6302c33f0dba905a23f21159
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-07 16:45:43 +0300

    NewEx_010

---
+++ Example_010/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {11,2,4,123,443,5,6,72,4,8};
+
+int n = array.Length;
+int find =4;
+
+int index = 0;
+
+while(index < n){
+    if ( array[index] == find){
+        Console.WriteLine(index);
+    }
+    index++;
+}
