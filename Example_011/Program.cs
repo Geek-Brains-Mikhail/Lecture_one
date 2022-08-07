b312df7d06602d3f85428d5f3728576277477b9e
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-07 16:45:59 +0300

    NewEx_011

---
+++ Example_011/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+void FILLArray(int[] collection){
+    int leght = collection.Length;
+    int index = 0;
+    while(index< leght){
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int [] collection,  int find){
+    int count = collection.Length;
+    int index = 0;
+    int position= 0;
+    while( index < count){
+        if(collection[index] == find){
+                position = index;
+                break;
+                
+        }
+        else{
+            position = -1;
+        }
+        
+        index++;
+    }
+    return position;
+}
+int [] MyArray = new int [10];
+FILLArray(MyArray);
+PrintArray(MyArray);
+System.Console.WriteLine();
+
+int pos = IndexOf(MyArray,4);
+System.Console.WriteLine(pos);
