51e0b4a8b16681b82ec48c3a7a229dab1861e188
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-07 16:45:19 +0300

    new_009_Ex

---
+++ Example_009(IntroArray)/Program.cs
@@ -0,0 +1,22 @@
+﻿int Findind(int [] Array, int find){
+    int score = 0;
+    int result = 0;
+    while(score < Array.Length){
+        if (Array[score] == find){
+            result = score;
+            break;
+            
+        }
+        else{
+            result = -1;
+        }
+        score++;
+    }
+    return result;
+}
+
+int[] array = {12,22,33,43,35,67,37,81,39};
+int find =37;
+int index = Findind(array,find);
+Console.WriteLine("Индекс схожего элемента = " + index);
+
